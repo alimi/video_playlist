e7d3463ae96e6ded2a7564b365de45c625983719
Ali Ibrahim <aibrahim2k2@gmail.com>
2011-08-10 23:43:07 -0400

    Adding rspec test

--- spec/models/playlist_spec.rb
+++ spec/models/playlist_spec.rb
@@ -1,5 +1,41 @@
 require 'spec_helper'
 
 describe Playlist do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @attr = {
+      :name => "test",
+      :youtube_id => "2FDD934D493C1893"
+    }
+  end
+
+  it "should create a new instance given valid attributes" do
+    Playlist.create!(@attr)
+  end
+
+  it "should require a name" do
+    no_name = Playlist.new(@attr.merge(:name => ""))
+    no_name.should_not be_valid
+  end
+
+  it "should have a unique name" do
+    Playlist.create!(@attr)
+    duplicate_name = Playlist.new(@attr.merge(:youtube_id => "48BEBC3F884FC594"))
+    duplicate_name.should_not be_valid
+  end
+
+  it "should require a youtube id" do
+    no_id = Playlist.new(@attr.merge(:youtube_id => ""))
+    no_id.should_not be_valid
+  end
+
+  it "should have an active youtube id" do
+    invalid_id = Playlist.new(@attr.merge(:youtube_id => "trash"))
+    invalid_id.should_not be_valid
+  end
+
+  it "should have a unique youtube id" do
+    Playlist.create!(@attr)
+    duplicate_id = Playlist.new(@attr.merge(:name => "test_two"))
+    duplicate_id.should_not be_valid
+  end  
 end
